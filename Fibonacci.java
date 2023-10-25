15b1a7cea982dc90949d2c4031ea0e89bba72ee1
Vijay Verma <121660419+Vijayveema@users.noreply.github.com>
2023-10-25 19:29:06 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Fibonacci {
+    public static void main(String[] args) {
+        
+     int n1=0,n2=1, n3;
+    
+    
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        int num=sc.nextInt();
+    
+        System.out.print(n1+" "+n2);
+        for(int i=2;i<num;i++)
+        {
+         n3= n1+n2;
+         System.out.print(" "+n3);
+         n1=n2;
+         n2=n3;
+       
+      }
+    }
+}
